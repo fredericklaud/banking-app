7fc1afd9bff37425e71ca4444a99805b74d533c2
Frederick Laud Amoah-Darko Jr. <freddie@Fredericks-MacBook-Pro-2.local>
2026-04-02 16:12:56 -0400

    First commit.

---
+++ src/main/java/edu/sec/banking/model/Account.java
@@ -0,0 +1,76 @@
+package edu.sec.banking.model;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Account {
+    private Long id;
+    private String accHolderName;
+    private Double accountBalance;
+    private String accountType;
+
+    private static final String DEFAULT_ACCOUNT_TYPE = "CHECKING";
+
+
+    // Starting at 1000000000000000 to enforce the 16 digit
+    private static final AtomicLong counter = new AtomicLong(1000000000000000L);
+
+    public static long nextId() {
+        return counter.getAndIncrement();
+    }
+
+    public Account(){
+        this.id = nextId();
+        this.accountType = DEFAULT_ACCOUNT_TYPE;
+        this.accHolderName = "Account Holder";
+        this.accountBalance = 0.0;
+    }
+
+    public String getAccHolderName() {
+        return accHolderName;
+    }
+
+    public void setAccHolderName(String accHolderName) {
+        this.accHolderName = accHolderName;
+    }
+
+    public Double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(Double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Account account)) return false;
+        return Objects.equals(id, account.id) && Objects.equals(accHolderName, account.accHolderName) && Objects.equals(accountType, account.accountType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, accHolderName, accountType);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", accHolderName='" + accHolderName + '\'' +
+                ", accountType='" + accountType + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/edu/sec/banking/service/AccountService.java
@@ -0,0 +1,18 @@
+package edu.sec.banking.service;
+
+import edu.sec.banking.model.Account;
+
+public class AccountService {
+
+    public Account createAccount(){
+        return null;
+    }
+
+    public Account updateAccount(){
+        return null;
+    }
+
+    public void deleteAccount(){
+        
+    }
+}
