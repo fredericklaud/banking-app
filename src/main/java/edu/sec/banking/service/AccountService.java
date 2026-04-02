0322ce79cfcf2d32d3bc96aebbada80ea4aadaca
Frederick Laud Amoah-Darko Jr. <freddie@Fredericks-MacBook-Pro-2.local>
2026-04-02 19:58:29 -0400

    implemented createAccount and updateAccount methods in the AccountService class

--- src/main/java/edu/sec/banking/service/AccountService.java
+++ src/main/java/edu/sec/banking/service/AccountService.java
@@ -2,17 +2,53 @@ package edu.sec.banking.service;
 
 import edu.sec.banking.model.Account;
 
+import java.util.ArrayList;
+
 public class AccountService {
 
-    public Account createAccount(){
-        return null;
+    public ArrayList<Account> accounts = new ArrayList<>();
+
+    public Account createAccount(Account account){
+        Account newAccount = new Account();
+        if (account != null){
+            newAccount.setAccountType(account.getAccountType());
+            newAccount.setAccHolderName(account.getAccHolderName());
+            newAccount.setAccountBalance(account.getAccountBalance());
+
+            accounts.add(newAccount);
+        }
+        else {
+            System.err.println("account is null");
+            return null;
+        }
+        return newAccount;
     }
 
-    public Account updateAccount(){
-        return null;
+    public boolean updateAccount(Account account){
+        boolean updateSucces = false;
+        if (account != null){
+            for (Account acc: accounts) {
+                if (acc.getId() == account.getId()){
+                    acc.setAccountBalance(account.getAccountBalance());
+                    acc.setAccHolderName(account.getAccHolderName());
+                    acc.setAccountType(account.getAccountType());
+
+                    // replacing old account details in the account array with new details
+                    int accIndex = accounts.indexOf(acc);
+                    accounts.set(accIndex, acc);
+                    updateSucces = true;
+                    break;
+                }
+            }
+        }
+        return updateSucces;
     }
 
     public void deleteAccount(){
-        
+
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
     }
 }
